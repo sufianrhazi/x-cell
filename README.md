8334a03bfa9ac366de2ee428c5f5df2a8a26c728
Sufian Rhazi <sufian@gmail.com>
2024-11-21 17:11:43 -0500

    no need for https

--- README.md
+++ README.md
@@ -4,4 +4,8 @@ Everyone puts complex CSS and HTML in JavaScript these days.
 
 `x-cell` lets you put some simple JavaScript in your HTML!
 
+This is _probably_ a joke.
 
+## Running
+
+To run locally, you'll need nodejs, (ideally [nvm](https://github.com/nvm-sh/nvm))
--- vite.config.js
+++ vite.config.js
@@ -14,14 +14,4 @@ export default defineConfig({
     define: {
         VERSION: '"dev"',
     },
-    server: {
-        https: {
-            key: fs.readFileSync('./localhost.key').toString(),
-            cert: fs.readFileSync('./localhost.crt').toString(),
-            ciphers:
-                'ECDHE-RSA-AES256-GCM-SHA384:ECDHE-RSA-AES128-GCM-SHA256:ECDHE-RSA-AES128-SHA:ECDHE-RSA-AES128-SHA256:ECDHE-RSA-AES256-SHA:ECDHE-RSA-AES256-SHA384',
-            honorCipherOrder: true,
-            secureProtocol: 'TLSv1_2_method',
-        },
-    },
 });
